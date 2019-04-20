37838dd40dcc81ab248b30ae27f7a5f371c9f9c7
AdhyCondotel <adhycondotel.makassar@gmail.com>
2019-04-20 20:04:11 +0800

    push

--- myapi/urls.py
+++ myapi/urls.py
@@ -1,18 +1,17 @@
 from django.urls import include, path
 from rest_framework import routers
-from rest_framework_jwt.views import (obtain_jwt_token, refresh_jwt_token,
-                                      verify_jwt_token)
+from rest_framework_jwt.views import (
+    obtain_jwt_token,
+    refresh_jwt_token,
+    verify_jwt_token
+    )
 
 from .views import (
     AddressView, 
     TenantView, 
     UserCreate, 
-    CustomJWTSerializer
+    CustomObtainJSONWebToken
 )
-
-from rest_framework_jwt.views import ObtainJSONWebToken
-
-
 
 router = routers.DefaultRouter()
 router.register(r'address', AddressView),
@@ -21,12 +20,8 @@ router.register(r'tenant', TenantView),
 urlpatterns = [
     path('', include(router.urls)),
     #path(r'login' , obtain_jwt_token),
-    path(r'login', ObtainJSONWebToken.as_view(serializer_class=CustomJWTSerializer)),
-    path("register/", UserCreate.as_view(), name="register"),
+    path(r'login', CustomObtainJSONWebToken.as_view()),
+    path(r'register', UserCreate.as_view()),
     path(r'token-refresh', refresh_jwt_token),
     path(r'token-verify', verify_jwt_token),
-
-
-    
-
 ]
--- myapi/views.py
+++ myapi/views.py
@@ -1,8 +1,15 @@
+from rest_framework_jwt.compat import PasswordField
+from rest_framework_jwt.views import ObtainJSONWebToken
+from rest_framework_jwt.settings import api_settings
+from django.utils.translation import ugettext as _
+from django.contrib.auth import authenticate, get_user_model
+from rest_framework_jwt.serializers import JSONWebTokenSerializer
+from django.db.models import Q
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from latihanDjango.utils.paginations import CustomResultsSetPagination
 from rest_framework import viewsets
 from rest_framework import generics
-from rest_framework import status
+from rest_framework import status, exceptions
 from rest_framework.response import Response
 from myapi.models import *
 from myapi.serializers import *
@@ -35,7 +42,62 @@ jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 jwt_decode_handler = api_settings.JWT_DECODE_HANDLER
 jwt_get_username_from_payload = api_settings.JWT_PAYLOAD_GET_USERNAME_HANDLER
 
+
+
+class CustomJWTException(exceptions.APIException):
+    status_code = 401
+    default_detail = 'Bad Request.'
+    # default_code = 'service_unavailable'
+
+
 class CustomJWTSerializer(JSONWebTokenSerializer):
+    username_field = 'username_or_email'
+    username_or_email = serializers.CharField(required=False)
+    password = PasswordField(write_only=True, required=False)
+
+
+    def validate(self, attrs):
+        password = attrs.get("password")
+        user_obj = User.objects.filter(
+            Q(username=attrs.get('username_or_email')) | Q(email=attrs.get('username_or_email'))).first()
+        msg = None
+        if user_obj is not None:
+            credentials = {
+                'username': user_obj.username,
+                'password': password
+            }
+            if all(credentials.values()):
+                user = authenticate(**credentials)
+                if user:
+                    if not user.is_active:
+                        msg = _('User account is disabled.')
+
+                    payload = jwt_payload_handler(user)
+
+                    return {
+                        'token': jwt_encode_handler(payload),
+                        'user': user
+                    }
+                else:
+                    msg = _('Unable to log in with provided credentialss.')
+
+            else:
+                msg = _('Must include "{username_field}" and "password".')
+                msg = msg.format(username_field=self.username_field)
+
+        else:
+            msg = _('Account with this email/username does not exists')
+
+        if msg:
+            raise CustomJWTException(msg)
+
+        return attrs
+
+
+class CustomObtainJSONWebToken(ObtainJSONWebToken):
+    serializer_class = CustomJWTSerializer
+
+# class CustomJWTSerializer(JSONWebTokenSerializer):
     username_field = 'username_or_email'
     def validate(self, attrs):
         password = attrs.get("password")
@@ -68,9 +130,11 @@ class CustomJWTSerializer(JSONWebTokenSerializer):
                 raise serializers.ValidationError(msg)
 
         else:
-            msg = _('Account with this email/username does not exists')
-            #return Response({"detail" : "eroor", "status_code": status.HTTP_400_BAD_REQUEST}, status=status.HTTP_400_BAD_REQUEST)
-            raise serializers.ValidationError(msg)
+            res = {"code": 400, "message": "Bad Requset"}
+            return Response(res)
+                        # msg = _('Must include "{username_field}" and "password".')
+            # msg = msg.format(username_field=self.username_field)
+            # raise serializers.ValidationError(msg)
 
 class TenantView(viewsets.ModelViewSet):
     queryset = Tenant.objects.all()
